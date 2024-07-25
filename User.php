51ef5a2ffcb7fb5dc3bc6d5ad86ff6083397fe1f
abdelrhman <abdelrhmanroshdy8@gmail.com>
2024-07-25 17:30:02 +0300

    Add access modifiers and getter/setter methods to User class.

--- User.php
+++ User.php
@@ -2,5 +2,27 @@
 
 class User
 {
+    // Access Modifier
+    // Public    ->  the property or method can be accessed from everywhere. This is default
+    // protected -> the property or method can be accessed within the class and by classes derived from that class
+    // private   -> the property or method can ONLY be accessed within the class
+    //-----------------------------------------------------------------------------//
+    // $this keyword ->  a reserved keyword in PHP that refers to the calling object.
+    private $name;
+    /**
+     * @param mixed $name
+     * @return User::name
+     */
+    public function setName($name) : void
+    {
+        $this->name = $name;
+    }
 
+    /**
+     * @return mixed
+     */
+    public function getName() : string
+    {
+        return $this->name;
+    }
 }
