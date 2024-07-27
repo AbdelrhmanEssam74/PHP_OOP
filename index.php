60367962b7ddd72643775678e443725acc56c1a8
abdelrhman <abdelrhmanroshdy8@gmail.com>
2024-07-27 18:14:52 +0300

    Add car objects for BMW and Mercedes to index.php
    - Instantiate BMW and Mercedes classes
    - Set color and company properties for each vehicle
    - Display object information using print_r
    - Comment out unused var_dump statements for clarity

--- index.php
+++ index.php
@@ -3,6 +3,25 @@ require_once 'User.php';
 $user = new User(); // Create a new Instance of User object
 $user->setName("Abdo");
 echo "<pre>";
-var_dump(User::$title);
-var_dump(User::instance());
+//var_dump(User::$title);
+//var_dump(User::instance());
+echo "</pre>";
+
+//------------------------------------------------------------------//
+//-------------------------Inheritance------------------------------//
+
+require_once 'BMW.php';
+$bmwCar = new BMW();
+$bmwCar->setColor("blueBlack");
+$bmwCar->setCompany("BMW");
+echo "<pre>";
+print_r($bmwCar);
+echo "</pre>";
+
+require_once 'mercedes.php';
+$mercedesCar = new mercedes();
+$mercedesCar->setColor("darkGray");
+$mercedesCar->setCompany("Mercedes");
+echo "<pre>";
+print_r($mercedesCar);
 echo "</pre>";
