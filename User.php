31e51461b9841c61d5b3011a4acc3c0d5020ba0b
abdelrhman <abdelrhmanroshdy8@gmail.com>
2024-07-27 16:26:43 +0300

    Refactor User class to include static properties and methods, updating documentation on self and static keywords.

--- User.php
+++ User.php
@@ -9,7 +9,6 @@ class User
     //-----------------------------------------------------------------------------//
     // $this keyword ->  a reserved keyword in PHP that refers to the calling object.
     private ?string $name = null;
-
     // Typed Properties
     public ?int $age = null; // Uninitialized
     //  Default value for property of type int may not be null, so Use the nullable type ?int to allow null default value
@@ -28,5 +27,12 @@ class User
     public function getName() : string
     {
         return $this->name;
+    }
+    // Self Keyword -> used to access static or class variables or methods and this is used to access non-static or object variables or methods
+    //The static keyword is used to declare properties and methods of a class as static. Static properties and methods can be used without creating an instance of the class.
+    //The static keyword is also used to declare variables in a function which keep their value after the function has ended.
+    public static string $title = 'OOP';
+    public static function instance() : static{
+        return new self;
     }
 }
