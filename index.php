17750a93687512fa4e0af9f548b41bf12f3c986f
abdelrhman <abdelrhmanroshdy8@gmail.com>
2024-07-31 16:29:16 +0300

    Remove debug output and add database creation logic for SQLite and MySQL. create two classes to handel different database drivers(mysql, sqlite)

--- index.php
+++ index.php
@@ -2,10 +2,10 @@
 require_once 'User.php';
 $user = new User(); // Create a new Instance of User object
 $user->setName("Abdo");
-echo "<pre>";
+//echo "<pre>";
 //var_dump(User::$title);
 //var_dump(User::instance());
-echo "</pre>";
+//echo "</pre>";
 
 //------------------------------------------------------------------//
 //-------------------------Inheritance------------------------------//
@@ -14,14 +14,27 @@ require_once 'BMW.php';
 $bmwCar = new BMW();
 $bmwCar->setColor("blueBlack");
 $bmwCar->setCompany("BMW");
-echo "<pre>";
-print_r($bmwCar);
-echo "</pre>";
+//echo "<pre>";
+//print_r($bmwCar);
+//echo "</pre>";
 
 require_once 'mercedes.php';
 $mercedesCar = new mercedes();
 $mercedesCar->setColor("darkGray");
-$mercedesCar->setCompany("Mercedes");
-echo "<pre>";
-print_r($mercedesCar);
-echo "</pre>";
+//$mercedesCar->setCompany("Mercedes");
+//echo "<pre>";
+//print_r($mercedesCar);
+//echo "</pre>";
+
+//------------------------------------------------------------------//
+//-------------------------Encapsulation----------------------------//
+require_once 'Database.php';
+require_once 'SQLiteHandler.php';
+require_once 'MySQLHandler.php';
+
+$db = new Database();
+$db->handle(new SQLiteHandler());
+$db->create(); // Creating sqlite database
+
+$db->handle(new MySQLHandler());
+$db->create(); // Creating mysql database
